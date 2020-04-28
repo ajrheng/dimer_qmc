22acd999f2038088d4fae566bff6d2e913a18183
Alvin Heng <alvin_heng95@hotmail.com>
2020-04-29 01:59:18 +0800

    explanation text for run files

--- run/dimer_script.py
+++ run/dimer_script.py
@@ -1,18 +1,22 @@
+"""
+Python script to run multiple jobs on a high-performance cluster, specifically those utilizing a PBS scheduler.
+"""
 import os
 import numpy as np
 from shutil import copy
 import time
 
+j1 = 0.1
 j2 = 0
 beta = 10
 l = 10
 
 cwd = os.getcwd()
-for j in np.arange(0,2,0.2):
-    folder_name = "J="+str(j)
+for h in np.arange(0,2,0.2):
+    folder_name = "h="+str(h)
     folder_path = os.path.join(cwd,folder_name)
     driver = os.path.join(cwd,'sse_driver.py')
-    script = os.path.join(cwd,'script.sh')
+    script = os.path.join(cwd,'script.txt')
     os.mkdir(folder_path)
     copy(driver,folder_path)
     copy(script,folder_path)
@@ -21,11 +25,12 @@ for j in np.arange(0,2,0.2):
     file = open('params.txt','a')
     file.write(str(j)+'\n')
     file.write(str(j2)+'\n')
+    file.write(str(h)+'\n')
     file.write(str(beta)+'\n')
     file.write(str(l))
     file.close()
 
-    os.system('qsub script.sh')
+    os.system('qsub script.txt')
     time.sleep(1) #to ensure current time seed is different for different simulations
 
     os.chdir(cwd)
--- run/sse_driver.py
+++ run/sse_driver.py
@@ -1,23 +1,27 @@
+"""
+example driver code to run a QMC simulation of the dimer model
+"""
 import numpy as np
 import sys
 
 #sys.path.append('/home/users/ntu/aheng004/dimer/python/src')
-sys.path.append('/home/alvin/Desktop/RA/python/src')
+sys.path.append('/path/to/src')
 
 from mc_sse_dimer import *
 
 if __name__ == "__main__":
 
     param = np.loadtxt('params.txt')
-    if len(param) != 4:
+    if len(param) != 5:
         sys.exit("insufficient parameters")
 
     j1 = float(param[0])
     j2 = float(param[1])
-    beta = float(param[2])
-    L = int(param[3])
+    h = float(param[2])
+    beta = float(param[3])
+    L = int(param[4])
 
 
-    test = mc_sse_dimer(j1,j2,beta,L)
+    test = mc_sse_dimer(j1,j2,h,beta,L)
     test.main()
     
